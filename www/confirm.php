a9436de18b0b46a9f92cfbb78ce13b47f1bbd3bb
Unknown <BuHHu.com@mail.ru>
2018-02-09 23:20:32 +0300

    Добавил проверку

--- www/confirm.php
+++ www/confirm.php
@@ -1,18 +1,21 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 include ("bd.php");
-    $result = mysql_query("SELECT user_id FROM users WHERE login='$login'",$db); // извлекаем идентификатор пользователя с данным логином
+mysql_query("SET NAMES 'utf8'",$db);
+$registration_code = $_GET['registration_code'];
+$login = $_GET['login'];
+    $result = mysql_query("SELECT `user_id`,`login`,`registration_code` FROM `users` WHERE `login`='$login'",$db); // извлекаем идентификатор пользователя с данным логином
 	$myrow = mysql_fetch_array($result); 
-	$registration_code = $myrow['registration_code']; // создаем такой же код подтверждения
+	$registration_code1 = $myrow['registration_code']; // создаем такой же код подтверждения
 
-	if ($registration_code == $registration_code) // сравниваем полученный из url и сгенерированный код 
+	if ($registration_code == $registration_code1) // сравниваем полученный из url и сгенерированный код 
 		{
-		mysql_query("UPDATE  users SET activation='1' WHERE login='$login'",$db); // если равны, то активируем пользователя
-		echo "Ваш Email подтвержден! Теперь вы можете зайти на сайт под своим логином! <a href='http://kapitalm.com.xn--80aauktf0a4f.xn--80aswg/Auto.php'>Авторизация</a>";
+			mysql_query("UPDATE  `users` SET `activation`='1' WHERE `login`='$login'",$db); // если равны, то активируем пользователя
+			echo "Ваш Email подтвержден! Теперь вы можете зайти на сайт под своим логином! <a href='http://kapitalm.com.xn--80aauktf0a4f.xn--80aswg/Auto.php'>Авторизация</a>";
 		}
 	else
 		{
-		echo "Ошибка! Ваш Email не подтвержден! <a href='index.php'>Главная страница</a>";
-		// если же полученный из url и сгенерированный код не равны, то выдаем ошибку
+			echo "Ошибка! Ваш Email не подтвержден! <a href='index.php'>Главная страница</a>";
+			// если же полученный из url и сгенерированный код не равны, то выдаем ошибку
 		}
 ?>
