3b838771bd2535b1eaae382a509abbcfd645c430
Unknown <BuHHu.com@mail.ru>
2018-03-25 15:57:52 +0300

    Добавление консультантов

---
+++ www/registrcons.php
@@ -0,0 +1,75 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Консультанты</title>
+<SCRIPT type="text/javascript">
+
+function validate_form ( )
+{
+	valid = true;
+    
+        if ( document.Registr.login.value.length < 5 )
+        {
+                alert ( "Логин должен быть не короче 5 символов!" );
+                valid = false;
+        }
+    
+        if ( document.Registr.login.value.length > 15 )
+        {
+                alert ( "Логин не должен быть длиннее 10 символов!" );
+                valid = false;
+        }
+        
+    
+        if ( document.Registr.login.value == "")
+        {
+                alert ( "Введите логин!" );
+                valid = false;
+        }
+        
+        if ( document.Registr.email.value == "")
+        {
+                alert ( "Введите email!" );
+                valid = false;
+        }
+        
+        if ( document.Registr.password.value == "")
+        {
+                alert ( "Введите пароль!" );
+                valid = false;
+        }
+    
+        if ( document.Registr.password.value.length < 5)
+        {
+                alert ( "Пароль должен быть не короче 5 символов!" );
+                valid = false;
+        }
+
+        return valid;    
+}
+
+</SCRIPT>
+</head>
+<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+<body>
+<div>
+<div>
+<Center>
+<a href="/" title="Капитал-М" class="logo"><img src="images/km1.png" alt="Капитал-М" /></a>
+<h1>Введите данные консультанта</h1>
+<form name="Registr" action="save_cons.php" method="post" onsubmit="return validate_form( );" >
+<p><label>Введите логин:<br>
+<input name="login" size="30" type="text" placeholder="Логин"></label></p>
+<p><label>Введите email:<br>
+<input name="email" size="30" type="email" placeholder="mail@mail.ru"></label></p>
+<p><label>Введите пароль:<br>
+<input name="password" size="30" type="password" placeholder="Пароль"></label></p>
+<p><input name="Registr" type="submit" value="Добавить"></p>
+<br>
+<p><a href='/list_cons.php'>Назад</a></p>
+</form>
+</Center>
+</div>
+</div>
+</body>
+</html>
---
+++ www/save_cons.php
@@ -0,0 +1,64 @@
+﻿<?php
+    if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+    if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
+    //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
+    if (isset($_POST['email'])) { $email=$_POST['email']; if ($email =='') { unset($email);} }
+    //заносим введенный пользователем email в переменную $email, если он пустой, то уничтожаем переменную
+    if (empty($login) or empty($password)or empty($email)) //если пользователь не ввел логин,пароль или email то выдаем ошибку и останавливаем скрипт
+    {
+    echo "<script>alert('Вы ввели не всю информацию! Заполните все поля!');location.href='/registr.php';</script>";
+    }
+    if (!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $email)) //проверка е-mail адреса регулярным выражением на корректность
+    {
+    echo "<script>alert('Email введен неверно!');location.href='/registr.php';</script>";
+    }
+    //if (strlen($login) < 5 or strlen($login) > 15) { // проверка логина на количество символов
+    //echo "<script>alert('Логин должен состоять не менее чем из 5 символов и не более чем из 15!');location.href='/registr.php';</script>";
+    //}
+    //if (strlen($password) < 5 or strlen($password) > 15) { // проверка пароля на количество символов
+    //echo "<script>alert('Пароль должен состоять не менее чем из 5 символов и не более чем из 15!');location.href='/registr.php';</script>";
+    //}  
+    // Символы, которые будут использоваться в коде. 
+    $chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; 
+    // Количество символов в коде. 
+    $max=10; 
+    // Определяем количество символов в $chars 
+    $size=StrLen($chars)-1; 
+    // Определяем пустую переменную, в которую и будем записывать символы. 
+    $activation_code=null; 
+    // Создаём пароль. 
+    while($max--) 
+    $activation_code.=$chars[rand(0,$size)]; 
+    // Выводим созданный код.
+    // если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+    // удаляем лишние пробелы
+    $email = htmlspecialchars(stripslashes($email));
+    $login = trim(htmlspecialchars(stripslashes($login)));
+    $password = strrev(md5(htmlspecialchars(stripslashes($password)))); //шифрую пароль, для надежности добавил реверс
+    $role_id = "consultant";
+ // подключаемся к базе
+    include ("bd.php");
+    include ("config.php");
+ // проверка на существование пользователя с таким же логином
+    $result = mysql_query("SELECT user_id FROM users WHERE login='$login'",$db);
+    $myrow = mysql_fetch_array($result);
+    if (!empty($myrow['user_id'])) 
+    {
+    echo "<script>alert('Введенный вами логин уже зарегистрирован! Введите другой логин!');location.href='/registr.php';</script>";
+    }
+ // если такого нет, то сохраняем данные
+    $result2 = mysql_query ("INSERT INTO users (login,password,email,activation_code,role_id,time_reg) VALUES('$login','$password','$email','$activation_code','$role_id',NOW())");
+    // Проверяем, есть ли ошибки
+    if ($result2=='TRUE')
+    {
+$subject= " Письмо с сайта kapitalm.com ";
+$message= $login.",спасибо за регистрацию на сайте ООО Капитал-М. Чтобы продолжить пройдите по ссылке $site_server_name/confirm.php?activation_code=$activation_code&login=$login";
+mail($email,$subject,$message);
+        if (empty($myrow['activation_code'])) 
+    {
+echo "<script>alert('Консультант добавлен! Для продолжения подтвердите аккаунт консультанта на почте!');location.href='/registrcons.php';</script>";
+        }
+} 
+else 
+    echo "Ошибка при отправке"
+    ?>
