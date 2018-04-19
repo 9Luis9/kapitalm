74b95ac1667f705e5b57963a27a4e19c660276d5
Unknown <BuHHu.com@mail.ru>
2018-04-19 15:00:07 +0300

    файл с добавлением новых сообщений

---
+++ www/check_chat.php
@@ -0,0 +1,22 @@
+<?php
+include ("bd.php");
+    $Query = mysql_query("SELECT `user_id`,`message_time`,`message_text`,`login`,`role_id`,`uploaded_doc` FROM `messages` LEFT JOIN `users` USING (`user_id`) WHERE `chat_id` = '$myrow[chat_id]' ORDER By `message_time` DESC LIMIT 30");
+    while ($myrow = mysql_fetch_array($Query))
+            {
+            if ($myrow['role_id']=='client' && (!empty($myrow['uploaded_doc'])))
+            {
+              echo '<div class="ChatBlock"><span>'.$myrow['login'].' | '.$myrow['message_time'].'</span>'.$myrow['message_text'].'&nbsp<a href="doc/'.$myrow['uploaded_doc'].'">Документ</a></div>';   
+            }
+            elseif ($myrow['role_id']=='client' && (empty($myrow['uploaded_doc'])))
+            {
+                echo '<div class="ChatBlock"><span>'.$myrow['login'].' | '.$myrow['message_time'].'</span>'.$myrow['message_text'].'</div>';  
+            } 
+            if ($myrow['role_id']=='consultant'&& (!empty($myrow['uploaded_doc'])))
+            {
+              echo '<div class="Consultant"><span>'.$myrow['login'].' | '.$myrow['message_time'].'</span>'.$myrow['message_text'].'&nbsp<a href="doc/'.$myrow['uploaded_doc'].'">Документ</a></div>';   
+            }
+            elseif ($myrow['role_id']=='consultant' && (empty($myrow['uploaded_doc']))){
+                echo '<div class="Consultant"><span>'.$myrow['login'].' | '.$myrow['message_time'].'</span>'.$myrow['message_text'].'</div>';  
+            } 
+            }
+?>
